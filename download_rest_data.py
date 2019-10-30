abfc6dfa2b8c0a998721d51501ad8e18e85b5e7b
Gabighz <ghiuzangabriel2@gmail.com>
2019-10-30 17:35:20 +0000

    Changed rest data file

--- download_rest_data.py
+++ download_rest_data.py
@@ -2,5 +2,5 @@ import urllib.request
 
 print('Beginning file download with urllib2...')
 
-url = 'https://hcp-rest-data.s3.eu-west-2.amazonaws.com/rest.nii'
-urllib.request.urlretrieve(url, 'rest.nii')
+url = 'https://hcp-rest-data.s3.eu-west-2.amazonaws.com/rest-phaseOne.nii.gz'
+urllib.request.urlretrieve(url, 'rest-phaseOne.nii.gz')
