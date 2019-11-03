f5ccabcc9e6def6152471702314b3bd221121feb
Gabighz <ghiuzangabriel2@gmail.com>
2019-11-03 18:32:03 +0000

    Updated download script

--- download_rest_data.py
+++ download_rest_data.py
@@ -2,5 +2,8 @@ import urllib.request
 
 print('Beginning file download with urllib2...')
 
-url = 'https://hcp-rest-data.s3.eu-west-2.amazonaws.com/rest-phaseOne.nii.gz'
+url = 'https://hcp-wumann-data.s3.eu-west-2.amazonaws.com/rest-phaseOne.nii.gz'
 urllib.request.urlretrieve(url, 'rest-phaseOne.nii.gz')
+
+url = 'https://hcp-wumann-data.s3.eu-west-2.amazonaws.com/task.nii.gz'
+urllib.request.urlretrieve(url, 'task.nii.gz')
